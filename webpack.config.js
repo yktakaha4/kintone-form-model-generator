f25f60c80a437a442a86b942eb872f543f555841
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-01-08 23:40:25 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
