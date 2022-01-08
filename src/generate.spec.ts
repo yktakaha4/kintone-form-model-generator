2a6b4fbfc610825285b986cc42dcc2f2e09a2feb
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-01-08 23:21:45 +0900

    add test for parameter

--- src/generate.spec.ts
+++ src/generate.spec.ts
@@ -46,7 +46,9 @@ describe("generate", () => {
         clientConfig,
       });
 
-      expect(result).toContain("interface Kintone入力項目テストアプリRecord");
+      expect(result).toContain(
+        "interface Kintone入力項目テストアプリRecord extends"
+      );
     });
 
     test("modelNameMapping", async () => {
@@ -60,7 +62,10 @@ describe("generate", () => {
         clientConfig,
       });
 
-      expect(result).toContain("interface KintoneCustomNameRecord");
+      expect(result).toContain("interface KintoneCustomNameRecord extends");
+      expect(result).toContain(
+        "interface KintoneCustomNameRecordForParameter {"
+      );
     });
 
     test("if modelNamePrefix is empty", async () => {
@@ -72,7 +77,8 @@ describe("generate", () => {
         clientConfig,
       });
 
-      expect(result).toContain("interface App54Record");
+      expect(result).toContain("interface App54Record extends");
+      expect(result).toContain("interface App54RecordForParameter {");
     });
 
     test("if modelNameSuffix is empty", async () => {
@@ -84,7 +90,8 @@ describe("generate", () => {
         clientConfig,
       });
 
-      expect(result).toContain("interface KintoneApp54");
+      expect(result).toContain("interface KintoneApp54 extends");
+      expect(result).toContain("interface KintoneApp54ForParameter {");
     });
   });
 
@@ -175,9 +182,17 @@ describe("generate", () => {
         clientConfig,
       });
 
-      expect(result).toContain("interface Kintone入力項目テストアプリRecord");
       expect(result).toContain(
-        "interface Kintone入力項目テストアプリRecordApp55"
+        "interface Kintone入力項目テストアプリRecord extends"
+      );
+      expect(result).toContain(
+        "interface Kintone入力項目テストアプリRecordApp55 extends"
+      );
+      expect(result).toContain(
+        "interface Kintone入力項目テストアプリRecordForParameter {"
+      );
+      expect(result).toContain(
+        "interface Kintone入力項目テストアプリRecordApp55ForParameter {"
       );
     });
   });
@@ -213,21 +228,25 @@ export interface KintoneApp54Record extends Record {
     "__REVISION__": Revision;
     /**
     * カテゴリー
+    * CATEGORY
     * @type Category
     */
     "カテゴリー": Category;
     /**
     * グループ選択
+    * GROUP_SELECT
     * @type GroupSelect
     */
     "グループ選択": GroupSelect;
     /**
     * ステータス
+    * STATUS
     * @type Status
     */
     "ステータス": Status;
     /**
     * チェックボックス
+    * CHECK_BOX
     * @type CheckBox
     */
     "チェックボックス": CheckBox;
@@ -239,121 +258,360 @@ export interface KintoneApp54Record extends Record {
         /**
         * 文字列 (1行)
         * 文字列__1行__Table
+        * SINGLE_LINE_TEXT
         * @type SingleLineText
         */
         "文字列__1行__Table": SingleLineText;
     }>;
     /**
     * ドロップダウン
+    * DROP_DOWN
     * @type Dropdown
     */
     "ドロップダウン": Dropdown;
     /**
     * ユーザー選択
+    * USER_SELECT
     * @type UserSelect
     */
     "ユーザー選択": UserSelect;
     /**
     * ラジオボタン
+    * RADIO_BUTTON
     * @type RadioButton
     */
     "ラジオボタン": RadioButton;
     /**
     * リッチエディター
+    * RICH_TEXT
     * @type RichText
     */
     "リッチエディター": RichText;
     /**
     * リンク
+    * LINK
     * @type Link
     */
     "リンク": Link;
     /**
     * ルックアップ
+    * NUMBER
     * @type Number
     */
     "ルックアップ": Number;
     /**
     * レコード番号
+    * RECORD_NUMBER
     * @type RecordNumber
     */
     "レコード番号": RecordNumber;
     /**
     * 作成日時
+    * CREATED_TIME
     * @type CreatedTime
     */
     "作成日時": CreatedTime;
     /**
     * 作成者
+    * CREATOR
     * @type Creator
     */
     "作成者": Creator;
     /**
     * 作業者
+    * STATUS_ASSIGNEE
     * @type StatusAssignee
     */
     "作業者": StatusAssignee;
     /**
     * 数値
+    * NUMBER
     * @type Number
     */
     "数値": Number;
     /**
     * 文字列 (1行)
     * 文字列__1行_
+    * SINGLE_LINE_TEXT
     * @type SingleLineText
     */
     "文字列__1行_": SingleLineText;
     /**
     * 文字列 (複数行)
     * 文字列__複数行_
+    * MULTI_LINE_TEXT
     * @type MultiLineText
     */
     "文字列__複数行_": MultiLineText;
     /**
     * 日付
+    * DATE
     * @type Date
     */
     "日付": Date;
     /**
     * 日時
+    * DATETIME
     * @type DateTime
     */
     "日時": DateTime;
     /**
     * 時刻
+    * TIME
     * @type Time
     */
     "時刻": Time;
     /**
     * 更新日時
+    * UPDATED_TIME
     * @type UpdatedTime
     */
     "更新日時": UpdatedTime;
     /**
     * 更新者
+    * MODIFIER
     * @type Modifier
     */
     "更新者": Modifier;
     /**
     * 添付ファイル
+    * FILE
     * @type File
     */
     "添付ファイル": File;
     /**
     * 組織選択
+    * ORGANIZATION_SELECT
     * @type OrganizationSelect
     */
     "組織選択": OrganizationSelect;
     /**
     * 複数選択
+    * MULTI_SELECT
     * @type MultiSelect
     */
     "複数選択": MultiSelect;
     /**
     * 計算
+    * CALC
     * @type Calc
     */
     "計算": Calc;
+}
+/**
+* KintoneApp54RecordForParameter
+* 入力項目テストアプリ
+* id: 54
+* revision: 4
+* @see ${dummyBaseUrl}/k/54/
+*/
+export interface KintoneApp54RecordForParameter {
+    /**
+    * グループ選択
+    * GROUP_SELECT
+    * @type Object
+    */
+    "グループ選択"?: {
+        value: {
+            code: string;
+        }[];
+    };
+    /**
+    * チェックボックス
+    * CHECK_BOX
+    * @type Object
+    */
+    "チェックボックス"?: {
+        value: string[];
+    };
+    /**
+    * テーブル
+    * SUBTABLE
+    * @type Object
+    */
+    "テーブル"?: {
+        value: {
+            /**
+            * id
+            * @type string
+            */
+            id: string;
+            value: {
+                /**
+                * 文字列 (1行)
+                * 文字列__1行__Table
+                * SINGLE_LINE_TEXT
+                * @type SingleLineText
+                */
+                "文字列__1行__Table"?: {
+                    value: string;
+                };
+            };
+        }[];
+    };
+    /**
+    * ドロップダウン
+    * DROP_DOWN
+    * @type Object
+    */
+    "ドロップダウン"?: {
+        value: string;
+    };
+    /**
+    * ユーザー選択
+    * USER_SELECT
+    * @type Object
+    */
+    "ユーザー選択"?: {
+        value: {
+            code: string;
+        }[];
+    };
+    /**
+    * ラジオボタン
+    * RADIO_BUTTON
+    * @type Object
+    */
+    "ラジオボタン"?: {
+        value: string;
+    };
+    /**
+    * リッチエディター
+    * RICH_TEXT
+    * @type Object
+    */
+    "リッチエディター"?: {
+        value: string;
+    };
+    /**
+    * リンク
+    * LINK
+    * @type Object
+    */
+    "リンク"?: {
+        value: string;
+    };
+    /**
+    * ルックアップ
+    * NUMBER
+    * @type Object
+    */
+    "ルックアップ"?: {
+        value: string;
+    };
+    /**
+    * 作成日時
+    * CREATED_TIME
+    * @type Object
+    */
+    "作成日時"?: {
+        value: string;
+    };
+    /**
+    * 作成者
+    * CREATOR
+    * @type Object
+    */
+    "作成者"?: {
+        value: {
+            code: string;
+        };
+    };
+    /**
+    * 数値
+    * NUMBER
+    * @type Object
+    */
+    "数値"?: {
+        value: string;
+    };
+    /**
+    * 文字列 (1行)
+    * 文字列__1行_
+    * SINGLE_LINE_TEXT
+    * @type Object
+    */
+    "文字列__1行_"?: {
+        value: string;
+    };
+    /**
+    * 文字列 (複数行)
+    * 文字列__複数行_
+    * MULTI_LINE_TEXT
+    * @type Object
+    */
+    "文字列__複数行_"?: {
+        value: string;
+    };
+    /**
+    * 日付
+    * DATE
+    * @type Object
+    */
+    "日付"?: {
+        value: string;
+    };
+    /**
+    * 日時
+    * DATETIME
+    * @type Object
+    */
+    "日時"?: {
+        value: string;
+    };
+    /**
+    * 時刻
+    * TIME
+    * @type Object
+    */
+    "時刻"?: {
+        value: string;
+    };
+    /**
+    * 更新日時
+    * UPDATED_TIME
+    * @type Object
+    */
+    "更新日時"?: {
+        value: string;
+    };
+    /**
+    * 更新者
+    * MODIFIER
+    * @type Object
+    */
+    "更新者"?: {
+        value: {
+            code: string;
+        };
+    };
+    /**
+    * 添付ファイル
+    * FILE
+    * @type Object
+    */
+    "添付ファイル"?: {
+        value: {
+            fileKey: string;
+        }[];
+    };
+    /**
+    * 組織選択
+    * ORGANIZATION_SELECT
+    * @type Object
+    */
+    "組織選択"?: {
+        value: {
+            code: string;
+        }[];
+    };
+    /**
+    * 複数選択
+    * MULTI_SELECT
+    * @type Object
+    */
+    "複数選択"?: {
+        value: string[];
+    };
 }
 `.trimStart();
