e66629a3cde9e431ea812cee0e6f621306d41bae
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-01-09 14:36:55 +0900

    fix cli

--- src/util/args.ts
+++ src/util/args.ts
@@ -2,14 +2,13 @@ import yargs from "yargs";
 
 export const parseArgs = (argv: Array<string>) => {
   return yargs(argv)
-    .command("generate", "Generate command", (yargs) => {
-      return yargs.option("app-ids", {
-        alias: "a",
-        type: "string",
-        description: "Kintone app id list. (Separated by comma)",
-      });
+    .command("generate", "Generate type definitions.")
+    .demandCommand(1, 1)
+    .option("app-ids", {
+      alias: "a",
+      type: "string",
+      description: "Kintone app id list. (Separated by comma)",
     })
-    .demandCommand(1)
     .option("config", {
       alias: "c",
       type: "string",
